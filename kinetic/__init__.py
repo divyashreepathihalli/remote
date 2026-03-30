ade15d094e5e671bbc4376f71105c539c1637e9d
divyashreepathihalli <divyashreepathihalli@gmail.com>
2026-03-30 18:51:40 +0000

    add version

--- kinetic/__init__.py
+++ kinetic/__init__.py
@@ -17,6 +17,8 @@ from kinetic.data import Data as Data
 from kinetic.jobs import JobHandle as JobHandle
 from kinetic.jobs import attach as attach
 from kinetic.jobs import list_jobs as list_jobs
+from kinetic.version import __version__ as __version__
+from kinetic.version import version as version
 
 logging.use_absl_handler()
 
